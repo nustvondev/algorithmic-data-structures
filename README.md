873cf5ea3eb2c9446e0c97404a2f53a750745e7c
Nguyễn Hữu Hòa <vjetnodestudio@gmail.com>
2023-10-09 19:59:53 +0000

    Duyệt mảng.

---
+++ README.md
@@ -0,0 +1,17 @@
+### Mảng
+
+*   Mảng là một tập hợp các phần tử có cùng kiểu dữ liệu(trừ javascript :>)
+*   Mảng có thể phân loại dựa theo số chiều: mảng 1 chiều, mảng 2 chiều…
+
+Cách dùng:
+
+```java
+int[] nums = {1,2,3,4,5}; // khai báo trường hợp đã biết phần tử ban đầu
+int[] nums = new int[5]; // khai báo trường hợp chưa biết trước	
+```
+
+Thao tác với mảng:
+
+*   Duyệt mảng
+*   Thêm phần tử vào mảng
+*   Xóa phần tử trong mảng
---
+++ leetcode/_1295_Find_Numbers_with_Even_Number_of_Digits.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+public class _1295_Find_Numbers_with_Even_Number_of_Digits {
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int i : nums) {
+            int numOfDigits = calNumOfDigits(i);
+            if (numOfDigits % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int calNumOfDigits(int i) {
+        // implement 1: divide 10
+        // example:
+        // 2345/10= 234 (remainder 3)
+        // 234/10= 23 (remainder 4)
+        // 23/10=2 (remainder 3)
+        // 2/10=0 (remainder 2) ==> STOP
+
+        // implement 2: Logarit 10
+        // return (int)Math.floor(Math.log10(i)) + 1;
+
+        int result = i;
+        int count = 0;
+        while (result != 0) {
+            result = i / 10;
+            i = result;
+            count++;
+        }
+        // while (n != 0) {
+        //     n = n / 10;
+        //     count++;
+        // }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] test_1 = { 12, 345, 2, 6, 7896 };// 2
+        int[] test_2 = { 555, 901, 482, 177 };// 0
+        int[] test_3 = { 12, 345, 2, 6, 7896 };// 2
+        int[] test_4 = { 555, 901, 482, 1771 };// 1
+
+        System.out.println(findNumbers(test_1));
+        System.out.println(findNumbers(test_2));
+        System.out.println(findNumbers(test_3));
+        System.out.println(findNumbers(test_4));
+
+    }
+}
